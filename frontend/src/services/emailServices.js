0c64dd410da5d49c7a0014151e3316bc581be757
ogunlewe <ogunlewealameen08@gmail.com>
2025-04-03 07:03:35 +0100

    commit

--- frontend/src/services/emailServices.js
+++ frontend/src/services/emailServices.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL =  'https://igbogbounion-backend.vercel.app/api';
 
 export const sendWelcomeEmail = async (email) => {
   try {
